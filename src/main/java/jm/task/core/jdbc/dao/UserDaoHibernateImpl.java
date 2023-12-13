368e5b739057c8df93612b7b7d578183fccadc97
pro100n00b <127980856+pro100n00b@users.noreply.github.com>
2023-12-13 17:06:07 +0400

    update

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,113 +1,120 @@
 package jm.task.core.jdbc.dao;
 
-
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
-
-import javax.persistence.PersistenceException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+
+    private final Util ut = new Util();
     public UserDaoHibernateImpl() {
 
     }
 
 
+
+
     @Override
     public void createUsersTable() {
-        Util ut = new Util();
-        Session se = ut.getSessionFactory().getCurrentSession();
-        se.getTransaction().begin();
-        String sql = "CREATE TABLE IF NOT EXISTS users " +
-                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
-                "age TINYINT NOT NULL)";
+        try (Session se = ut.getSessionFactory().openSession()) {
+            se.getTransaction().begin();
+            String sql = "CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                    "age TINYINT NOT NULL)";
 
-        Query query = se.createSQLQuery(sql).addEntity(User.class);
-        query.executeUpdate();
-        se.getTransaction().commit();
-        se.close();
+            Query query = se.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+            se.getTransaction().commit();
+        } catch (HibernateException ignored) {
+
+        }
 
 
     }
 
     @Override
     public void dropUsersTable() {
-        Util ut = new Util();
-        Session se = ut.getSessionFactory().getCurrentSession();
-        se.getTransaction().begin();
-        String sql = "DROP TABLE IF EXISTS users";
+        try (Session se = ut.getSessionFactory().openSession()) {
+            se.getTransaction().begin();
+            String sql = "DROP TABLE IF EXISTS users";
+            Query query = se.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+            se.getTransaction().commit();
+        } catch (HibernateException ignored) {
 
-        Query query = se.createSQLQuery(sql).addEntity(User.class);
-        query.executeUpdate();
-        se.getTransaction().commit();
-        se.close();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Util ut = new Util();
-        Session se = ut.getSessionFactory().getCurrentSession();
-        se.getTransaction().begin();
-        se.save(new User(name, lastName,age));
-        se.getTransaction().commit();
-        se.close();
+        try (Session se = ut.getSessionFactory().openSession()) {
+            se.getTransaction().begin();
+            se.save(new User(name, lastName, age));
+            se.getTransaction().commit();
+        } catch (HibernateException ignored) {
+
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Util ut = new Util();
-        Session se = ut.getSessionFactory().getCurrentSession();
-        se.getTransaction().begin();
-        se.createQuery("delete from User where id = :id")
-                .setParameter("id", id)
-                .executeUpdate();;
-        se.getTransaction().commit();
-        se.close();
+        try (Session se = ut.getSessionFactory().openSession()) {
+            se.getTransaction().begin();
+            se.createQuery("delete from User where id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            ;
+            se.getTransaction().commit();
+        } catch (HibernateException ignored) {
+
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        Util ut = new Util();
-        Session se = ut.getSessionFactory().getCurrentSession();
-        se.getTransaction().begin();
-        CriteriaBuilder criteriaBuilder = se.getCriteriaBuilder();
-        CriteriaQuery<User> query = criteriaBuilder.createQuery(User.class);
-        Root<User> root = query.from(User.class);
+        try (Session se = ut.getSessionFactory().openSession()) {
+            se.getTransaction().begin();
+            CriteriaBuilder criteriaBuilder = se.getCriteriaBuilder();
+            CriteriaQuery<User> query = criteriaBuilder.createQuery(User.class);
+            Root<User> root = query.from(User.class);
 
-        query.select(root);
+            query.select(root);
 
-        List<User> resultList = se.createQuery(query).getResultList();
-        se.getTransaction().commit();
-        se.close();
-        return resultList;
+            List<User> resultList = se.createQuery(query).getResultList();
+            se.getTransaction().commit();
+            return resultList;
+        } catch (HibernateException ignored) {
+
+        }
+        return null;
+
 
     }
 
     @Override
     public void cleanUsersTable() {
-        Util ut = new Util();
-        Session se = ut.getSessionFactory().getCurrentSession();
-        se.getTransaction().begin();
-        try {
+        try (Session se = ut.getSessionFactory().openSession()) {
+
+            se.getTransaction().begin();
+
             String sql = "TRUNCATE TABLE users";
             Query query = se.createSQLQuery(sql).addEntity(User.class);
             query.executeUpdate();
             se.getTransaction().commit();
-            se.close();
-        }catch (PersistenceException e){
+        } catch (HibernateException ignored) {
 
         }
-
-
-
 
 
     }
